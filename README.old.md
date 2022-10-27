e8aaf0818bf0cfefa091c5a804f015c5c3a089da
khairnsaa <k.nsaa95@gmail.com>
2022-10-27 21:53:50 +0700

    done

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# khairunnisaas-mini-test
+mini test week 2 assignment sparta coding club bootcamp
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [ task, setTask ] = useState('')
+    const [ taskList, setTaskList] = useState([])
+
+    const addTask = (e) => {
+        e.preventDefault()
+        setTaskList([...taskList, task])
+        setTask('')
+    }
+
+    const getTime = () => {
+        const time = new Date().getHours()
+        let greetings = ''
+
+        if((time).toString() === '00' || (time).toString() <= '12') greetings = 'Good Morning 🌥'
+        else if((time).toString() === '12' || (time).toString() < '18') greetings = 'Good Afternoon ☀'
+        else if((time).toString() === '18' || (time).toString() <= '23') greetings = 'Good Evening 🌙'
+
+        return greetings
+    }
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Hey, {getTime()}</h1>
+                <p>What do you want to do today?</p>
+                <form className="input-todo" onSubmit={addTask}>
+                    <input type="text" placeholder='I Want To Do....' value={task} onChange={(e) => setTask(e.target.value)} />
+                    <button>Add</button>
+                </form>
+            </header>
+            <section className="task-list">
+                <ul>
+                    {
+                        taskList.map(task => (
+                            <li>{task}</li>
+                        ))
+                    }
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default App;
